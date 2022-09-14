35553c5b96abc24b8a6fa7daf696c988679025e6
yangzhiqin <zhiqin.yang.f@gmail.com>
2022-09-14 10:37:04 +0800

    add generic

---
+++ generic/ttl.go
@@ -0,0 +1,301 @@
+package generic
+
+import (
+	"runtime"
+	"sync"
+	"time"
+)
+
+type entry[K comparable, V any] struct {
+	value      V
+	key        K
+	Expiration int64
+}
+
+const (
+	// For use with functions that take an expiration time.
+	NoExpiration time.Duration = -1
+	// For use with functions that take an expiration time. Equivalent to
+	// passing in the same expiration duration as was given to New() or
+	// NewFrom() when the cache was created (e.g. 5 minutes.)
+	DefaultExpiration time.Duration = 0
+)
+
+type Cache[K comparable, V any] struct {
+	*cache[K, V]
+	// If this is confusing, see the comment at the bottom of New()
+}
+
+type cache[K comparable, V any] struct {
+	sync.RWMutex
+	defaultExpiration time.Duration
+	items             []entry[K, V]
+	indices           map[K]int
+	onEvicted         func(K, V)
+	stop              chan struct{}
+}
+
+// Add an item to the cache, replacing any existing item. If the duration is 0
+// (DefaultExpiration), the cache's default expiration time is used. If it is -1
+// (NoExpiration), the item never expires.
+func (c *cache[K, V]) AddWithTTL(k K, x V, d time.Duration) {
+	// "Inlining" of set
+	var e int64
+	if d == DefaultExpiration {
+		d = c.defaultExpiration
+	}
+	if d > 0 {
+		e = time.Now().Add(d).UnixNano()
+	}
+	c.Lock()
+	if idx, ok := c.indices[k]; ok {
+		c.items[idx].value = x
+		c.items[idx].key = k
+		c.items[idx].Expiration = e
+	} else {
+		idx := len(c.items)
+		c.items = append(c.items, entry[K, V]{key: k, value: x, Expiration: e})
+		c.indices[k] = idx
+	}
+	// TODO: Calls to mu.Unlock are currently not deferred because defer
+	// adds ~200 ns (as of go1.)
+	c.Unlock()
+}
+
+func (c *cache[K, V]) setWithTTL(k K, x V, d time.Duration) {
+	var e int64
+	if d == DefaultExpiration {
+		d = c.defaultExpiration
+	}
+	if d > 0 {
+		e = time.Now().Add(d).UnixNano()
+	}
+
+	if idx, ok := c.indices[k]; ok {
+		c.items[idx].value = x
+		c.items[idx].key = k
+		c.items[idx].Expiration = e
+	} else {
+		idx := len(c.items)
+		c.items = append(c.items, entry[K, V]{key: k, value: x, Expiration: e})
+		c.indices[k] = idx
+	}
+}
+
+// Add an item to the cache, replacing any existing item, using the default
+// expiration.
+func (c *cache[K, V]) Add(k K, x V) {
+	c.Lock()
+	c.setWithTTL(k, x, DefaultExpiration)
+	c.Unlock()
+}
+
+// Checks if a key exists in cache
+func (c *cache[K, V]) Contains(k K) bool {
+	c.RLock()
+	_, found := c.indices[k]
+	c.RUnlock()
+	return found
+}
+
+// Get an item from the cache. Returns the item or nil, and a bool indicating
+// whether the key was found.
+func (c *cache[K, V]) Get(k K) (v V, ok bool) {
+	c.RLock()
+	// "Inlining" of get and Expired
+	idx, found := c.indices[k]
+	if !found {
+		c.RUnlock()
+		return v, false
+	}
+
+	item := &c.items[idx]
+	if item.Expiration > 0 {
+		if time.Now().UnixNano() > item.Expiration {
+			c.RUnlock()
+			return v, false
+		}
+	}
+	c.RUnlock()
+	return item.value, true
+}
+
+// GetWithExpiration returns an item and its expiration time from the cache.
+// It returns the item or nil, the expiration time if one is set (if the item
+// never expires a zero value for time.Time is returned), and a bool indicating
+// whether the key was found.
+func (c *cache[K, V]) GetWithExpiration(k K) (v V, t time.Time, ok bool) {
+	c.RLock()
+	idx, found := c.indices[k]
+	if !found {
+		c.RUnlock()
+		return v, time.Time{}, false
+	}
+
+	item := &c.items[idx]
+	if item.Expiration > 0 {
+		if time.Now().UnixNano() > item.Expiration {
+			c.RUnlock()
+			return v, time.Time{}, false
+		}
+
+		// Return the item and the expiration time
+		c.RUnlock()
+		return item.value, time.Unix(0, item.Expiration), true
+	}
+
+	// If expiration <= 0 (i.e. no expiration time set) then return the item
+	// and a zeroed time.Time
+	c.RUnlock()
+	return item.value, time.Time{}, true
+}
+
+// Delete an item from the cache. Does nothing if the key is not in the cache.
+func (c *cache[K, V]) Remove(k K) {
+	c.Lock()
+	v, evicted := c.remove(k)
+	c.Unlock()
+	if evicted {
+		c.onEvicted(k, v)
+	}
+}
+
+func (c *cache[K, V]) remove(k K) (v V, ok bool) {
+	idx, found := c.indices[k]
+	if !found {
+		return
+	}
+	n := len(c.indices) - 1
+	c.items[n], c.items[idx] = c.items[idx], c.items[n]
+	c.indices[c.items[idx].key] = idx
+	delete(c.indices, k)
+	if c.onEvicted != nil {
+		x := c.items[n]
+		c.items = c.items[:n]
+		return x.value, true
+	}
+	c.items = c.items[:n]
+	return v, false
+}
+
+// Delete all expired items from the cache.
+func (c *cache[K, V]) Tidy() {
+	var ks []K
+	var vs []V
+
+	now := time.Now().UnixNano()
+	c.Lock()
+	for _, v := range c.items {
+		// "Inlining" of expired
+		if v.Expiration > 0 && now > v.Expiration {
+			_, evicted := c.remove(v.key)
+			if evicted {
+				ks = append(ks, v.key)
+				vs = append(vs, v.value)
+			}
+		}
+	}
+	c.Unlock()
+	for i := range ks {
+		c.onEvicted(ks[i], vs[i])
+	}
+}
+
+// Sets an (optional) function that is called with the key and value when an
+// item is evicted from the cache. (Including when it is deleted manually, but
+// not when it is overwritten.) Set to nil to disable.
+func (c *cache[K, V]) OnEvicted(f func(K, V)) {
+	c.Lock()
+	c.onEvicted = f
+	c.Unlock()
+}
+
+// Copies all unexpired items in the cache into a new map and returns it.
+func (c *cache[K, V]) Keys() []K {
+	var ks []K
+	c.RLock()
+	defer c.RUnlock()
+	now := time.Now().UnixNano()
+	for _, v := range c.items {
+		// "Inlining" of Expired
+		if v.Expiration > 0 {
+			if now > v.Expiration {
+				continue
+			}
+		}
+		ks = append(ks, v.key)
+	}
+	return ks
+}
+
+// Returns the number of items in the cache. This may include items that have
+// expired, but have not yet been cleaned up.
+func (c *cache[K, V]) Len() int {
+	c.RLock()
+	n := len(c.items)
+	c.RUnlock()
+	return n
+}
+
+// Vist all items from the cache.
+func (c *cache[K, V]) Foreach(fn func(k K, v V)) {
+	c.Lock()
+	for i := range c.items {
+		fn(c.items[i].key, c.items[i].value)
+	}
+	c.Unlock()
+}
+
+// Delete all items from the cache.
+func (c *cache[K, V]) Purge() {
+	c.Lock()
+	c.items = c.items[:0]
+	c.indices = make(map[K]int)
+	c.Unlock()
+}
+
+func (c *cache[K, V]) run(interval time.Duration) {
+	ticker := time.NewTicker(interval)
+	for {
+		select {
+		case <-ticker.C:
+			c.Tidy()
+		case <-c.stop:
+			ticker.Stop()
+			return
+		}
+	}
+}
+
+func newCache[K comparable, V any](de time.Duration) *cache[K, V] {
+	if de == 0 {
+		de = -1
+	}
+	c := &cache[K, V]{
+		defaultExpiration: de,
+		indices:           make(map[K]int),
+		stop:              make(chan struct{}),
+	}
+	return c
+}
+
+func newCacheWithJanitor[K comparable, V any](de time.Duration, ci time.Duration) *Cache[K, V] {
+	c := newCache[K, V](de)
+	C := &Cache[K, V]{c}
+	if ci > 0 {
+		go c.run(ci)
+		runtime.SetFinalizer(C, func(C *Cache[K, V]) {
+			close(C.cache.stop)
+		})
+	}
+	return C
+}
+
+// Return a new cache with a given default expiration duration and cleanup
+// interval. If the expiration duration is less than one (or NoExpiration),
+// the items in the cache never expire (by default), and must be deleted
+// manually. If the cleanup interval is less than one, expired items are not
+// deleted from the cache before calling c.DeleteExpired().
+func New[K comparable, V any](defaultExpiration, cleanupInterval time.Duration) *Cache[K, V] {
+	return newCacheWithJanitor[K, V](defaultExpiration, cleanupInterval)
+}
---
+++ generic/ttl_test.go
@@ -0,0 +1,546 @@
+//go:build go1.18
+// +build go1.18
+
+package generic
+
+import (
+	"runtime"
+	"strconv"
+	"sync"
+	"testing"
+	"time"
+)
+
+type TestStruct struct {
+	Num      int
+	Children []*TestStruct
+}
+
+func TestCache(t *testing.T) {
+	tc := New[string, any](DefaultExpiration, 0)
+
+	a, found := tc.Get("a")
+	if found || a != nil {
+		t.Error("Getting A found value that shouldn't exist:", a)
+	}
+
+	b, found := tc.Get("b")
+	if found || b != nil {
+		t.Error("Getting B found value that shouldn't exist:", b)
+	}
+
+	c, found := tc.Get("c")
+	if found || c != nil {
+		t.Error("Getting C found value that shouldn't exist:", c)
+	}
+
+	tc.AddWithTTL("a", 1, DefaultExpiration)
+	tc.AddWithTTL("b", "b", DefaultExpiration)
+	tc.AddWithTTL("c", 3.5, DefaultExpiration)
+
+	x, found := tc.Get("a")
+	if !found {
+		t.Error("a was not found while getting a2")
+	}
+	if x == nil {
+		t.Error("x for a is nil")
+	} else if a2 := x.(int); a2+2 != 3 {
+		t.Error("a2 (which should be 1) plus 2 does not equal 3; value:", a2)
+	}
+
+	x, found = tc.Get("b")
+	if !found {
+		t.Error("b was not found while getting b2")
+	}
+	if x == nil {
+		t.Error("x for b is nil")
+	} else if b2 := x.(string); b2+"B" != "bB" {
+		t.Error("b2 (which should be b) plus B does not equal bB; value:", b2)
+	}
+
+	x, found = tc.Get("c")
+	if !found {
+		t.Error("c was not found while getting c2")
+	}
+	if x == nil {
+		t.Error("x for c is nil")
+	} else if c2 := x.(float64); c2+1.2 != 4.7 {
+		t.Error("c2 (which should be 3.5) plus 1.2 does not equal 4.7; value:", c2)
+	}
+}
+
+func TestCacheTimes(t *testing.T) {
+	var found bool
+
+	tc := New[string, int](50*time.Millisecond, 1*time.Millisecond)
+	tc.AddWithTTL("a", 1, DefaultExpiration)
+	tc.AddWithTTL("b", 2, NoExpiration)
+	tc.AddWithTTL("c", 3, 20*time.Millisecond)
+	tc.AddWithTTL("d", 4, 70*time.Millisecond)
+
+	<-time.After(25 * time.Millisecond)
+	_, found = tc.Get("c")
+	if found {
+		t.Error("Found c when it should have been automatically deleted")
+	}
+
+	<-time.After(30 * time.Millisecond)
+	_, found = tc.Get("a")
+	if found {
+		t.Error("Found a when it should have been automatically deleted")
+	}
+
+	_, found = tc.Get("b")
+	if !found {
+		t.Error("Did not find b even though it was set to never expire")
+	}
+
+	_, found = tc.Get("d")
+	if !found {
+		t.Error("Did not find d even though it was set to expire later than the default")
+	}
+
+	<-time.After(20 * time.Millisecond)
+	_, found = tc.Get("d")
+	if found {
+		t.Error("Found d when it should have been automatically deleted (later than the default)")
+	}
+}
+
+func TestStorePointerToStruct(t *testing.T) {
+	tc := New[string, *TestStruct](DefaultExpiration, 0)
+	tc.AddWithTTL("foo", &TestStruct{Num: 1}, DefaultExpiration)
+	x, found := tc.Get("foo")
+	if !found {
+		t.Fatal("*TestStruct was not found for foo")
+	}
+	x.Num++
+	y, found := tc.Get("foo")
+	if !found {
+		t.Fatal("*TestStruct was not found for foo (second time)")
+	}
+
+	if y.Num != 2 {
+		t.Fatal("TestStruct.Num is not 2")
+	}
+}
+
+func TestAdd(t *testing.T) {
+	tc := New[string, string](DefaultExpiration, 0)
+	tc.AddWithTTL("foo", "bar", DefaultExpiration)
+	tc.AddWithTTL("foo", "baz", DefaultExpiration)
+
+}
+
+func TestReplace(t *testing.T) {
+	tc := New[string, string](DefaultExpiration, 0)
+
+	tc.AddWithTTL("foo", "bar", DefaultExpiration)
+
+}
+
+func TestDelete(t *testing.T) {
+	tc := New[string, any](DefaultExpiration, 0)
+	tc.AddWithTTL("foo", "bar", DefaultExpiration)
+	tc.Remove("foo")
+	x, found := tc.Get("foo")
+	if found {
+		t.Error("foo was found, but it should have been deleted")
+	}
+	if x != nil {
+		t.Error("x is not nil:", x)
+	}
+}
+
+func TestItemCount(t *testing.T) {
+	tc := New[string, any](DefaultExpiration, 0)
+	tc.AddWithTTL("foo", "1", DefaultExpiration)
+	tc.AddWithTTL("bar", "2", DefaultExpiration)
+	tc.AddWithTTL("baz", "3", DefaultExpiration)
+	if n := tc.Len(); n != 3 {
+		t.Errorf("Item count is not 3: %d", n)
+	}
+}
+
+func TestFlush(t *testing.T) {
+	tc := New[string, string](DefaultExpiration, 0)
+	tc.AddWithTTL("foo", "bar", DefaultExpiration)
+	tc.AddWithTTL("baz", "yes", DefaultExpiration)
+	tc.Purge()
+	x, found := tc.Get("foo")
+	if found {
+		t.Error("foo was found, but it should have been deleted")
+	}
+
+	if x != "" {
+		t.Error("x is not nil:", x)
+	}
+	x, found = tc.Get("baz")
+	if found {
+		t.Error("baz was found, but it should have been deleted")
+	}
+	if x != "" {
+		t.Error("x is not nil:", x)
+	}
+}
+
+func TestOnEvicted(t *testing.T) {
+	tc := New[string, any](DefaultExpiration, 0)
+	tc.AddWithTTL("foo", 3, DefaultExpiration)
+	if tc.onEvicted != nil {
+		t.Fatal("tc.onEvicted is not nil")
+	}
+	works := false
+	tc.OnEvicted(func(k string, v interface{}) {
+		if k == "foo" && v.(int) == 3 {
+			works = true
+		}
+		tc.AddWithTTL("bar", 4, DefaultExpiration)
+	})
+	tc.Remove("foo")
+	x, _ := tc.Get("bar")
+	if !works {
+		t.Error("works bool not true")
+	}
+	if x.(int) != 4 {
+		t.Error("bar was not 4")
+	}
+}
+
+func BenchmarkCacheGetExpiring(b *testing.B) {
+	benchmarkCacheGet(b, 5*time.Minute)
+}
+
+func BenchmarkCacheGetNotExpiring(b *testing.B) {
+	benchmarkCacheGet(b, NoExpiration)
+}
+
+func benchmarkCacheGet(b *testing.B, exp time.Duration) {
+	b.StopTimer()
+	tc := New[string, any](exp, 0)
+	tc.AddWithTTL("foo", "bar", DefaultExpiration)
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		tc.Get("foo")
+	}
+}
+
+func BenchmarkRWMutexMapGet(b *testing.B) {
+	b.StopTimer()
+	m := map[string]string{
+		"foo": "bar",
+	}
+	mu := sync.RWMutex{}
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		mu.RLock()
+		_, _ = m["foo"]
+		mu.RUnlock()
+	}
+}
+
+func BenchmarkRWMutexInterfaceMapGetStruct(b *testing.B) {
+	b.StopTimer()
+	s := struct{ name string }{name: "foo"}
+	m := map[interface{}]string{
+		s: "bar",
+	}
+	mu := sync.RWMutex{}
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		mu.RLock()
+		_, _ = m[s]
+		mu.RUnlock()
+	}
+}
+
+func BenchmarkRWMutexInterfaceMapGetString(b *testing.B) {
+	b.StopTimer()
+	m := map[interface{}]string{
+		"foo": "bar",
+	}
+	mu := sync.RWMutex{}
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		mu.RLock()
+		_, _ = m["foo"]
+		mu.RUnlock()
+	}
+}
+
+func BenchmarkCacheGetConcurrentExpiring(b *testing.B) {
+	benchmarkCacheGetConcurrent(b, 5*time.Minute)
+}
+
+func BenchmarkCacheGetConcurrentNotExpiring(b *testing.B) {
+	benchmarkCacheGetConcurrent(b, NoExpiration)
+}
+
+func benchmarkCacheGetConcurrent(b *testing.B, exp time.Duration) {
+	b.StopTimer()
+	tc := New[string, any](exp, 0)
+	tc.AddWithTTL("foo", "bar", DefaultExpiration)
+	wg := new(sync.WaitGroup)
+	workers := runtime.NumCPU()
+	each := b.N / workers
+	wg.Add(workers)
+	b.StartTimer()
+	for i := 0; i < workers; i++ {
+		go func() {
+			for j := 0; j < each; j++ {
+				tc.Get("foo")
+			}
+			wg.Done()
+		}()
+	}
+	wg.Wait()
+}
+
+func BenchmarkRWMutexMapGetConcurrent(b *testing.B) {
+	b.StopTimer()
+	m := map[string]string{
+		"foo": "bar",
+	}
+	mu := sync.RWMutex{}
+	wg := new(sync.WaitGroup)
+	workers := runtime.NumCPU()
+	each := b.N / workers
+	wg.Add(workers)
+	b.StartTimer()
+	for i := 0; i < workers; i++ {
+		go func() {
+			for j := 0; j < each; j++ {
+				mu.RLock()
+				_, _ = m["foo"]
+				mu.RUnlock()
+			}
+			wg.Done()
+		}()
+	}
+	wg.Wait()
+}
+
+func BenchmarkCacheGetManyConcurrentExpiring(b *testing.B) {
+	benchmarkCacheGetManyConcurrent(b, 5*time.Minute)
+}
+
+func BenchmarkCacheGetManyConcurrentNotExpiring(b *testing.B) {
+	benchmarkCacheGetManyConcurrent(b, NoExpiration)
+}
+
+func benchmarkCacheGetManyConcurrent(b *testing.B, exp time.Duration) {
+	// This is the same as BenchmarkCacheGetConcurrent, but its result
+	// can be compared against BenchmarkShardedCacheGetManyConcurrent
+	// in sharded_test.go.
+	b.StopTimer()
+	n := 10000
+	tc := New[string, any](exp, 0)
+	keys := make([]string, n)
+	for i := 0; i < n; i++ {
+		k := "foo" + strconv.Itoa(i)
+		keys[i] = k
+		tc.AddWithTTL(k, "bar", DefaultExpiration)
+	}
+	each := b.N / n
+	wg := new(sync.WaitGroup)
+	wg.Add(n)
+	for _, v := range keys {
+		go func(k string) {
+			for j := 0; j < each; j++ {
+				tc.Get(k)
+			}
+			wg.Done()
+		}(v)
+	}
+	b.StartTimer()
+	wg.Wait()
+}
+
+func BenchmarkCacheSetExpiring(b *testing.B) {
+	benchmarkCacheSet(b, 5*time.Minute)
+}
+
+func BenchmarkCacheSetNotExpiring(b *testing.B) {
+	benchmarkCacheSet(b, NoExpiration)
+}
+
+func benchmarkCacheSet(b *testing.B, exp time.Duration) {
+	b.StopTimer()
+	tc := New[string, any](exp, 0)
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		tc.AddWithTTL("foo", "bar", DefaultExpiration)
+	}
+}
+
+func BenchmarkRWMutexMapSet(b *testing.B) {
+	b.StopTimer()
+	m := map[string]string{}
+	mu := sync.RWMutex{}
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		mu.Lock()
+		m["foo"] = "bar"
+		mu.Unlock()
+	}
+}
+
+func BenchmarkCacheSetDelete(b *testing.B) {
+	b.StopTimer()
+	tc := New[string, any](DefaultExpiration, 0)
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		tc.AddWithTTL("foo", "bar", DefaultExpiration)
+		tc.Remove("foo")
+	}
+}
+
+func BenchmarkRWMutexMapSetDelete(b *testing.B) {
+	b.StopTimer()
+	m := map[string]string{}
+	mu := sync.RWMutex{}
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		mu.Lock()
+		m["foo"] = "bar"
+		mu.Unlock()
+		mu.Lock()
+		delete(m, "foo")
+		mu.Unlock()
+	}
+}
+
+func BenchmarkCacheSetDeleteSingleLock(b *testing.B) {
+	b.StopTimer()
+	tc := New[string, any](DefaultExpiration, 0)
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		tc.Lock()
+		tc.setWithTTL("foo", "bar", DefaultExpiration)
+		tc.remove("foo")
+		tc.Unlock()
+	}
+}
+
+func BenchmarkRWMutexMapSetDeleteSingleLock(b *testing.B) {
+	b.StopTimer()
+	m := map[string]string{}
+	mu := sync.RWMutex{}
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		mu.Lock()
+		m["foo"] = "bar"
+		delete(m, "foo")
+		mu.Unlock()
+	}
+}
+
+func BenchmarkDeleteExpiredLoop(b *testing.B) {
+	b.StopTimer()
+	tc := New[string, any](5*time.Minute, 0)
+	tc.Lock()
+	for i := 0; i < 100000; i++ {
+		tc.setWithTTL(strconv.Itoa(i), "bar", DefaultExpiration)
+	}
+	tc.Unlock()
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		tc.Tidy()
+	}
+}
+
+func TestGetWithExpiration(t *testing.T) {
+	tc := New[string, any](DefaultExpiration, 0)
+
+	a, expiration, found := tc.GetWithExpiration("a")
+	if found || a != nil || !expiration.IsZero() {
+		t.Error("Getting A found value that shouldn't exist:", a)
+	}
+
+	b, expiration, found := tc.GetWithExpiration("b")
+	if found || b != nil || !expiration.IsZero() {
+		t.Error("Getting B found value that shouldn't exist:", b)
+	}
+
+	c, expiration, found := tc.GetWithExpiration("c")
+	if found || c != nil || !expiration.IsZero() {
+		t.Error("Getting C found value that shouldn't exist:", c)
+	}
+
+	tc.AddWithTTL("a", 1, DefaultExpiration)
+	tc.AddWithTTL("b", "b", DefaultExpiration)
+	tc.AddWithTTL("c", 3.5, DefaultExpiration)
+	tc.AddWithTTL("d", 1, NoExpiration)
+	tc.AddWithTTL("e", 1, 50*time.Millisecond)
+
+	x, expiration, found := tc.GetWithExpiration("a")
+	if !found {
+		t.Error("a was not found while getting a2")
+	}
+	if x == nil {
+		t.Error("x for a is nil")
+	} else if a2 := x.(int); a2+2 != 3 {
+		t.Error("a2 (which should be 1) plus 2 does not equal 3; value:", a2)
+	}
+	if !expiration.IsZero() {
+		t.Error("expiration for a is not a zeroed time")
+	}
+
+	x, expiration, found = tc.GetWithExpiration("b")
+	if !found {
+		t.Error("b was not found while getting b2")
+	}
+	if x == nil {
+		t.Error("x for b is nil")
+	} else if b2 := x.(string); b2+"B" != "bB" {
+		t.Error("b2 (which should be b) plus B does not equal bB; value:", b2)
+	}
+	if !expiration.IsZero() {
+		t.Error("expiration for b is not a zeroed time")
+	}
+
+	x, expiration, found = tc.GetWithExpiration("c")
+	if !found {
+		t.Error("c was not found while getting c2")
+	}
+	if x == nil {
+		t.Error("x for c is nil")
+	} else if c2 := x.(float64); c2+1.2 != 4.7 {
+		t.Error("c2 (which should be 3.5) plus 1.2 does not equal 4.7; value:", c2)
+	}
+	if !expiration.IsZero() {
+		t.Error("expiration for c is not a zeroed time")
+	}
+
+	x, expiration, found = tc.GetWithExpiration("d")
+	if !found {
+		t.Error("d was not found while getting d2")
+	}
+	if x == nil {
+		t.Error("x for d is nil")
+	} else if d2 := x.(int); d2+2 != 3 {
+		t.Error("d (which should be 1) plus 2 does not equal 3; value:", d2)
+	}
+	if !expiration.IsZero() {
+		t.Error("expiration for d is not a zeroed time")
+	}
+
+	x, expiration, found = tc.GetWithExpiration("e")
+	if !found {
+		t.Error("e was not found while getting e2")
+	}
+	if x == nil {
+		t.Error("x for e is nil")
+	} else if e2 := x.(int); e2+2 != 3 {
+		t.Error("e (which should be 1) plus 2 does not equal 3; value:", e2)
+	}
+
+	idx, _ := tc.indices["e"]
+
+	if expiration.UnixNano() != tc.items[idx].Expiration {
+		t.Error("expiration for e is not the correct time")
+	}
+	if expiration.UnixNano() < time.Now().UnixNano() {
+		t.Error("expiration for e is in the past")
+	}
+}
